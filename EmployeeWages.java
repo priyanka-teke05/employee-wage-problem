9f151b04b8d170faee19da059d61f82467c80d37
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-28 13:22:18 +0530

    ADDED: calculate parttime fulltime wage using case

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,28 +1,33 @@
 
 public class EmployeeWages {
-	public static void main(String args[]){
+	//constants
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+
+	public static void main(String args[]) {
 		
 		//display welcome message
 		System.out.println("Welcome to Employee Wage Computation");
-		
-		int IS_PART_TIME=0;
-		int IS_FULL_TIME=1;
-		int EMP_RATE_PER_HOUR=20;
 		
 		//variables
 		int empHrs=0;
 		int empWage=0;
 		
-		//random function to generate 0 or 1
-		int empCheck = (int)Math.floor(Math.random()*10%2);
-		
+		//random function to generate 1 or 2
+		int empCheck = (int)Math.floor(Math.random()*10)%3;
+
 		//check employee is present or absent and assign empHrs
-		if(empCheck == IS_FULL_TIME)
-			empHrs=8;
-		else if(empCheck == IS_PART_TIME)
-			empHrs=4;
-		else
-			empHrs=0;
+		switch(empCheck) {
+			case IS_FULL_TIME:
+				empHrs=8;
+				break;
+			case IS_PART_TIME:
+				empHrs=4;
+				break;
+			default:
+				break;
+		}
 		
 		//calculate employee wage
 		empWage=empHrs*EMP_RATE_PER_HOUR;
