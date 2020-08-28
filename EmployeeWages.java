44261da1c189f0fc68215015ccab046b395fb4ca
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-28 15:03:48 +0530

    REFACTORED: create class method and variables

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -3,28 +3,32 @@ public class EmployeeWages {
 	//constants
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUM_OF_WORKING_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=100;
 	
-	public static void main(String args[]) {
-		
-		//display welcome message
-		System.out.println("Welcome to Employee Wage Computation");
-		
+	public final int empRatePerHr;
+	public final int numOfWorkingDays;
+	public final int maxHoursPerMonth;
+	
+	public EmployeeWages(int numOfWorkingDays, int empRatePerHr, int maxHoursPerMonth) {
+		this.empRatePerHr=empRatePerHr;
+		this.maxHoursPerMonth=maxHoursPerMonth;
+		this.numOfWorkingDays=numOfWorkingDays;
+	}
+	
+	public void employeeWage() {
 		//variables
 		int totalWage=0;
 		int totalHrs=0;
 		int totalDays=0;
 		
-		while(totalHrs<=MAX_HRS_IN_MONTH && totalDays<NUM_OF_WORKING_DAYS) {
+		while(totalHrs<=maxHoursPerMonth && totalDays<numOfWorkingDays) {
+			//local variable
 			int empHrs=0;
 			int empWage=0;
-			int empCheck = (int)Math.floor(Math.random()*10)%3;
 			
+			int empCheck = (int)Math.floor(Math.random()*10)%3;			
 			totalDays++;
 			
-			//check employee is parttime or fulltime and assign hours
+			//check employee is part time or full time and assign hours
 			switch(empCheck) {
 				case IS_FULL_TIME:
 					empHrs=8;
@@ -35,12 +39,22 @@ public class EmployeeWages {
 				default:
 					break;
 			}
+			
+			//computations
 			totalHrs=totalHrs+empHrs;
-			empWage=empHrs*EMP_RATE_PER_HOUR;
+			empWage=empHrs*empRatePerHr;
 			totalWage=totalWage+empWage;
 			System.out.println("Employee wage : day "+totalDays+" : "+empWage);
 		}
+		//display total hours and wages
 		System.out.println("Total Hours : "+totalHrs);	
 		System.out.println("Total wage : "+totalWage);	
+	}
+	public static void main(String args[]) {
+		
+		//display welcome message
+		System.out.println("Welcome to Employee Wage Computation");
+		EmployeeWages emp=new EmployeeWages(20,20,100);
+		emp.employeeWage();	
 	}
 }
