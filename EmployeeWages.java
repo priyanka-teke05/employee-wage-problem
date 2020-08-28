a96e040db70ba8fea8dfeba0ad4aea9d3f16ec2f
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-28 22:49:42 +0530

    REFACTORED: manage employee wage of multiple companies

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -8,9 +8,12 @@ public class EmployeeWages {
 		System.out.println("Welcome to Employee Wage Computation");
 		
 		EmpWageBuilder emp = new EmpWageBuilder();
+		
+		//different companies employee details
 		emp.empWageDetails("D-mart",20,20,100);
 		emp.empWageDetails("Baggit",20,20,100);
 		emp.empWageDetails("Reliance",20,20,100);
 		emp.empWageDetails("Big-Bazar",20,20,100);
+	
 	}
 }
